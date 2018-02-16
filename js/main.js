53b625755ebb8c323523385d6c48c01f08c2691a
Steven Haws <stevenjhaws@gmail.com>
2018-02-15 18:20:09 -0700

    got the js working... mostly

--- js/main.js
+++ js/main.js
@@ -4,28 +4,24 @@ document.getElementById("familybtn").addEventListener("click", family);
 document.getElementById("contactbtn").addEventListener("click", contact);
 
 function education(){
-  document.getElementById("hobbies").style.display = "none"
-  document.getElementById("family").style.display = "none"
-  document.getElementById("contact").style.display = "none"
-  document.getElementById("education").style.display = "initial"
-  console.log("hey")
-}
+  hideAll();
+  document.getElementById("education").style.display = "initial";
+};
 function hobbies(){
-  document.getElementById("family").style.display = "none"
-  document.getElementById("contact").style.display = "none"
-  document.getElementById("education").style.display = "none"
-  document.getElementById("hobbies").style.display = "initial"
-  console.log("hey")
-}
+  hideAll();
+  document.getElementById("hobbies").style.display = "initial";
+};
 function family(){
-  document.getElementById("hobbies").style.display = "none"
-  document.getElementById("contact").style.display = "none"
-  document.getElementById("education").style.display = "none"
-  document.getElementById("family").style.display = "initial"
-}
+  hideAll();
+  document.getElementById("family").style.display = "initial";
+};
 function contact(){
-  document.getElementById("hobbies").style.display = "none"
-  document.getElementById("family").style.display = "none"
-  document.getElementById("education").style.display = "none"
-  document.getElementById("contact").style.display = "initial"
+  hideAll();
+  document.getElementById("contact").style.display = "initial";
+};
+function hideAll(){
+  document.getElementById("hobbies").style.display = "none";
+  document.getElementById("family").style.display = "none";
+  document.getElementById("contact").style.display = "none";
+  document.getElementById("education").style.display = "none";
 }
